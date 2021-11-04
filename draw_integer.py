056413ccc155f949e6e77c9f3b8ee0e1e6054d0c
JooYoung Seo <jooyoung.j.seo@gmail.com>
2021-11-03 21:48:52 -0500

    Add a new function: average_integers()

--- draw_integer.py
+++ draw_integer.py
@@ -1,6 +1,8 @@
 import random
 
 # drawing_integers function
+
+
 def drawing_integers(lb, ub, trials):
     drawing_list = []
     for i in range(trials):
@@ -8,7 +10,20 @@ def drawing_integers(lb, ub, trials):
         drawing_list.append(n)
     return(drawing_list)
 
+
 # Test
 list1 = drawing_integers(1, 6, 20)
 
 print(list1)
+
+
+# 2. average_integers
+def average_integers(num_list):
+    sum = 0
+    for i in num_list:
+        sum = sum + i
+    average = sum/len(num_list)
+    return(average)
+
+# Test
+print(average_integers(list1))
